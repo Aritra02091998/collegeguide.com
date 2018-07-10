f13c3920c0ffccd14103a59f5c02f6d89db72c01
sourav <sinhasouravsinha1997@gmail.com>
2018-07-10 17:15:06 +0530

    project tab logo added

--- project_llb_page.php
+++ project_llb_page.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="shortcut icon" href="/project/img/shortlogo.png"/>
         <link rel="stylesheet" href="/project/css/bootstrap.css">
         <link rel="stylesheet" href="/project/css/college_details.css">
         <title>Collegeguide.com</title>
@@ -27,16 +28,11 @@
                         </div>
                         <div>
                             <p class="address">
-                            
-                            
-                            The University Law College, Bangalore University, popularly known as ULC - Bangalore, is a legal education institution located in the city of Bangalore, Karnataka, India. Wikipedia
-                        Address: Jnanabharathi Campus, Bengaluru, Karnataka 560056
-                        Affiliation: Bangalore University
-                        Principal: Prof.V Sudesh
-                        Founded: 1 July 1948
-                        Phone: 080 2339 2384
-
-            	            
+                                Address: Jnanabharathi Campus, Bengaluru, Karnataka 560056<br/>
+                                Affiliation: Bangalore University<br/>
+                                Principal: Prof.V Sudesh<br/>
+                                Founded: 1 July 1948<br/>
+                                Phone: 080 2339 2384
                             </p>
                          </div>
                     </div>
@@ -44,14 +40,16 @@
                         <p class="details">
                             <b style="color:red">Course</b><br/>
                             
-                            The University Law College, Bangalore University, popularly known as ULC - Bangalore, is a legal education institution located in the city of Bangalore, Karnataka, India. Wikipedia
+                            The University Law College, Bangalore University, popularly known as ULC - Bangalore,
+                            is a legal education institution located in the city of Bangalore, Karnataka, India. Wikipedia
                            
                             Official link---><a href="//ucbangalore.com">ucbangalore.com</a>
                         </p>
                     </div>                                    
                 </div>
             </div>
-            <br/><br/>                
+            <br/>
+            <br/>                
             <div>
                 <div>
                     <h3>The New Law College</h3>
@@ -64,15 +62,11 @@
                         <div>
                             <p class="address">
                             
-                            Founded: 1978
-                        Principal: Dr. Mukund Sarda
-                        Dean: Dr. Mukund Sarda
-                        Chancellor: Patangrao Kadam
-                        Number of students: 10,000
-
-                            
-                             
-            	             
+                                Founded: 1978
+                                Principal: Dr. Mukund Sarda
+                                Dean: Dr. Mukund Sarda
+                                Chancellor: Patangrao Kadam
+                                Number of students: 10,000            	             
                             </p>
                         </div>
                      </div>
@@ -81,7 +75,8 @@
                             <b style="color:red">Course</b><br/>
                             
                             
-                            <b>The New Law College</b> is a law college situated in Pune. It was established in 1978. New Law College has been a constituent college of the Bharati Vidyapeeth university since 1996.
+                            <b>The New Law College</b> is a law college situated in Pune. It was established in 1978.
+                             New Law College has been a constituent college of the Bharati Vidyapeeth university since 1996.
                             
                             Official link-><a href="//www.newlawcollege.com">www.newlawcollege.com</a>
                         </p>
@@ -99,37 +94,30 @@
                             <img class="college_pic" src="img/llb.jpg"/>
                         </div>
                         <div>
-                            <p class="address">
-                            
-                            
-                            
-                            
-                            
-                            Address: Senate House, University Road, Old Katra, Allahabad, Uttar Pradesh 211002
-                        Founded: 23 September 1887
-                        Chancellor: Goverdhan Mehta
-                        Affiliation: University Grants Commission
-                        Motto: "Quot Rami Tot Arbores" ("Every branch yields a Tree", Latin)
-                        Vice-chancellor: Rattan Lal Hangloo
-
-            	                
+                            <p class="address">                            
+                                Address: Senate House, University Road, Old Katra, Allahabad, Uttar Pradesh 211002
+                                Founded: 23 September 1887
+                                Chancellor: Goverdhan Mehta
+                                Affiliation: University Grants Commission
+                                Motto: "Quot Rami Tot Arbores" ("Every branch yields a Tree", Latin)
+                                Vice-chancellor: Rattan Lal Hangloo            	                
                             </p>
                         </div>
                      </div>
                      <div class="college_details">
                         <p class="details">
-                            <b style="color:red">Course</b><br/>
-                            
-                            
-                            
-                             The University of Allahabad, informally known as Allahabad University, is a public central university located in Allahabad, Uttar Pradesh, India. Established on 23 September 1887, it is the fourth oldest modern university in India. Wikipedia
-                              
-                            
-                            Official link-><a href="//www.allduniv.ac.in">www.allduniv.ac.in</a>
+                            <b style="color:red">Course</b><br/>                           
+                                The University of Allahabad, informally known as Allahabad University,
+                                is a public central university located in Allahabad, Uttar Pradesh, India.
+                                Established on 23 September 1887, it is the fourth oldest modern university in India. Wikipedia
+                                <br/>                            
+                                Official link-><a href="//www.allduniv.ac.in">www.allduniv.ac.in</a>
                         </p>
                      </div>                                    
                 </div>
-            </div>  
+            </div> 
+            <br/>
+            <br/> 
             <div>
                 <div>
                     <h3>The University of Delhi</h3>
@@ -141,30 +129,24 @@
                         </div>
                         <div>
                             <p class="address">
-                            
-                            
-                            
-                            
-                                  Address: Benito Juarez Rd, South Campus, South Moti Bagh, South Campus, South Moti Bagh, New Delhi, Delhi 110021
-                        Founded: 1922
-                        Vice-Chancellor: Professor Yogesh Tyagi
-                        Chancellor: Venkaiah Naidu
-                        Subsidiaries: Shri Ram College of Commerce, MORE
-                        Affiliations: University Grants Commission, MORE
-                        Suggest an edit
-            	                
+                                Address: Benito Juarez Rd, South Campus, South Moti Bagh, South Campus,
+                                South Moti Bagh, New Delhi, Delhi 110021
+                                Founded: 1922
+                                Vice-Chancellor: Professor Yogesh Tyagi
+                                Chancellor: Venkaiah Naidu
+                                Subsidiaries: Shri Ram College of Commerce, MORE
+                                Affiliations: University Grants Commission, MORE
+                                Suggest an edit           	                
                             </p>
                         </div>
                      </div>
                      <div class="college_details">
                         <p class="details">
-                            <b style="color:red">Course</b><br/>
-                            
-                            
-            	        The University of Delhi, informally known as Delhi University, is a collegiate public central university, located in New Delhi, India. It was founded in 1922 by an Act of the Central Legislative Assembly. Wikipedia
-                            
-                            
-                           
+                            <b style="color:red">Course</b><br/>                           
+            	            The University of Delhi, informally known as Delhi University,
+            	            is a collegiate public central university, located in New Delhi, India.
+            	            It was founded in 1922 by an Act of the Central Legislative Assembly. Wikipedia
+                            <br/>                           
             	            Official link-><a href="www.du.ac.in">www.du.ac.in</a>
                         </p>
                      </div>                                    
